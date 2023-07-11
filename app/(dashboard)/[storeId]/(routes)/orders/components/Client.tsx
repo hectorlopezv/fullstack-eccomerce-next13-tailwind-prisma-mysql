91f8c0213a44ee9ddf4e6d2ebf00ff4e33256b55
hector lopez <hectorvmlopez@gmail.com>
2023-07-11 12:08:48 -0500

    cleaning repo

--- app/(dashboard)/[storeId]/(routes)/orders/components/Client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/Client.tsx
@@ -1,21 +1,15 @@
 "use client";
 
 import Heading from "@/components/ui/Heading";
-import { Plus } from "lucide-react";
-import { Button } from "@/components/ui/Button";
 import { Separator } from "@/components/ui/Separator";
-import { useRouter } from "next/navigation";
-import { useParams } from "next/navigation";
 import { OrderColumn, columns } from "./Columns";
 import { DataTable } from "./data-table";
-import ApiList from "@/components/ui/api-list";
 type Props = {
   data: OrderColumn[];
 };
 
 export default function BillBoardClient({ data }: Props) {
-  const router = useRouter();
-  const params = useParams();
+ 
   return (
     <>
       <Heading
@@ -24,7 +18,7 @@ export default function BillBoardClient({ data }: Props) {
       />
 
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable columns={columns} data={data} searchKey="products" />
     </>
   );
 }
